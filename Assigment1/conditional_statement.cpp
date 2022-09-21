a2a9329386cca85e30778d14fd8d11bbeeacc657
mayanksingh2512 <107640144+mayanksingh2512@users.noreply.github.com>
2022-09-21 22:59:22 +0530

    Create conditional_statement.cpp

---
+++ Assigment1/conditional_statement.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a;
+    cin>>a;
+    if (a==1) {
+        cout<<"one";
+    }
+    if(a==2){
+        cout<<"two";
+    }
+    if (a==3){
+        cout<<"three";
+    }
+    if (a==4){
+        cout<<"four";
+    }
+    if (a==5) {
+        cout<<"five";
+    }
+    if (a==6) {
+    cout<<"six";
+    }
+    if (a==7){
+        cout<<"seven";
+    }
+    if (a==8){
+       cout<<"eight";
+    }
+    if (a==9) {
+       cout<<"nine";
+    }
+    
+    if (a>9) {
+       cout<<"Greater than 9";
+    }
+    return 0;
+}
