8903adf506d421e76b55fca93798573ff68990cb
mayanksingh2512 <107640144+mayanksingh2512@users.noreply.github.com>
2023-02-10 23:19:35 +0530

    Create birthday_cake.cpp

---
+++ Assignment 3/birthday_cake.cpp
@@ -0,0 +1,30 @@
+https://www.hackerrank.com/challenges/birthday-cake-candles/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    int max=0,count=0;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+     if(max<a[i])
+     {
+         max=a[i];
+     }
+    }
+    for(int i=0;i<n;i++){
+    if(a[i]==max)
+    {
+        count++;
+    }}
+    
+    cout<<count;
+}
